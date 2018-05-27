06574a1f006d04ad648b2848fda8c675317f680b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:28 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-import pipe from "@unction/pipe"
-
-export default function aside (unctions: Array<mixed => mixed>): Function {
-  const journey = pipe(unctions)
-
-  return function asideUnctions (value: mixed): mixed {
-    journey(value)
-
-    return value
-  }
+import pipe from "@unction/pipe";
+export default function aside(unctions) {
+  const journey = pipe(unctions);
+  return function asideUnctions(value) {
+    journey(value);
+    return value;
+  };
 }
