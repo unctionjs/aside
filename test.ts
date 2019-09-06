25eafa452b0102d9561762568d4f99ce024d6e49
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:43 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,34 @@
+
+import {spy} from "sinon";
+
+import aside from "./index.ts";
+
+test(() => {
+  const unction = spy(() => "b");
+
+  expect(aside([unction])("a")).toBe("a");
+});
+
+test(() => {
+  const unction = spy(() => "b");
+
+  aside([unction])("a");
+
+  expect(unction.calledWith("a")).toBeTruthy();
+});
+
+test(() => {
+  const unction = spy(() => "b");
+
+  expect(aside([unction])("a")).toBe("a");
+});
+
+test(() => {
+  const unctionA = spy(() => "b");
+  const unctionB = spy(() => "c");
+
+  expect(aside([unctionA, unctionB])("a")).toBe("a");
+
+  expect(unctionA.calledWith("a")).toBeTruthy();
+  expect(unctionB.calledWith("b")).toBeTruthy();
+});
